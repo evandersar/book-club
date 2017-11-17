b44981c33b783d9b511f671498842be7ce19160d
Ivan Zinchenko <evandersar@ukr.net>
2017-11-17 14:37:34 +0000

    login auth on back

--- back/index.js
+++ back/index.js
@@ -1,12 +1,25 @@
 'use strict';
 
 var path = process.cwd();
-var BookHandler = require(path + '/back/bookHandler.js');
+var passport = require('passport');
+
+var BookHandler = require(path + '/back/handlers/bookHandler.js');
+var UserHandler = require(path + '/back/handlers/userHandler.js');
 
 module.exports = function(app) {
-	
+
 	var bookHandler = new BookHandler();
-	
+	var userHandler = new UserHandler();
+
+	app.route('/api/signup')
+		.post(userHandler.signup);
+		
+	app.route('/api/authenticate')
+		.post(userHandler.authenticate);
+		
+	app.route('/api/memberinfo')
+		.get(userHandler.memberinfo, passport.authenticate('jwt', { session: false}) );
+
 	app.route('/api/items')
 		.post(bookHandler.searchBooks)
 		.get(bookHandler.getBooks);
--- server.js
+++ server.js
@@ -4,7 +4,12 @@ var express = require('express');
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+//var jwt = require('jwt-simple');
+
 var routes = require('./back/index.js');
+//var config = require('./back/config/database'); // get db config file
+//var User = require('./back/models/user'); // get the mongoose model
 
 var app = express();
 require('dotenv').config();
@@ -18,6 +23,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
+
+// Use the passport package in our application
+app.use(passport.initialize());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/front', express.static(process.cwd() + '/front'));
